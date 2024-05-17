75aa98fdbc227786ab62b357184fc6845a0a0209
Yelina-Daelemans <144011374+Yelina-Daelemans@users.noreply.github.com>
2024-05-17 19:51:19 +0200

    I forgot to change something back

--- Assets/Scripts/Actors/Action.cs
+++ Assets/Scripts/Actors/Action.cs
@@ -19,7 +19,7 @@ public class Action : MonoBehaviour
     }
     static private void EndTurn(Actor actor) 
     {
-        if(actor == GameManager.Get.player) 
+        if(actor.GetComponent<Player>() != null) 
         {
             GameManager.Get.StartEnemyTurn();
         }
